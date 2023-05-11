32eef3973287d3ebdc072285548688c3f5d02324
deep-poharkar <deeppoharkar21@gmail.com>
2023-05-11 12:18:39 +0530

    word replace code

---
+++ .history/wordReplace_20230511121734.py
@@ -0,0 +1,6 @@
+sentence = input('Enter your sentence : ')
+print('your sentence is ' + sentence)
+word1 = input('enter word you wanna replace ' )
+word2= input('enter word you want it to replace with ' )
+
+print (sentence.replace(word1,word2))
