4a028c6f44f10f82e4acf47663d36cf869f0e556
deep-poharkar <deeppoharkar21@gmail.com>
2023-05-11 17:44:23 +0530

    basic login and sign up logic

---
+++ basicSignupAndLogin.py
@@ -0,0 +1,18 @@
+print('Create an account')
+
+username1= input('Enter your username : ')
+password1= input('Enter your password: ')
+
+print('Account created successfully!')
+print('Login Now!')
+
+username2= input ('Enter your username : ')
+password2= input('Enter your password: ')
+
+
+if username1 == username2 and password1 == password2 :
+    print('Logged in successfully')
+    
+else :
+    print('Invalid username or password.')
+   
